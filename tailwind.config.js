c3968433771c23b1be7cefe702966e96ab3c7ded
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2023-09-21 00:37:51 +0530

    myfirst

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+
+module.exports = {
+
+  purge: [],
+
+  darkMode: false,   // or 'media' or 'class'
+
+   theme: {
+
+      extend: {},
+
+},
+
+  variants: {},
+
+  plugins: [],
+
+}
